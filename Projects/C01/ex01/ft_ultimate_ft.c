3beb0218f589c05d0f90435be1add07936c7d514
cwan42 <136786922+cwan42@users.noreply.github.com>
2023-06-23 15:19:02 +0800

    Create ft_ultimate_ft.c

---
+++ Projects/C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,24 @@
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+/*
+#include <stdio.h>
+
+int	main()
+{
+	int n = 1;
+	int *nbr1 = &n;
+	int **nbr2 = &nbr1;
+	int ***nbr3 = &nbr2;
+	int ****nbr4 = &nbr3;
+	int *****nbr5 = &nbr4;
+	int ******nbr6 = &nbr5;
+	int *******nbr7 = &nbr6;
+	int ********nbr8 = &nbr7;
+	int *********nbr = &nbr8;
+
+	ft_ultimate_ft(nbr);
+	printf("%d\n", n);
+}
+*/
