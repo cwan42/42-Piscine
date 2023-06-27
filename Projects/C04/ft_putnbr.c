66017b92cc8539867a236acae646cdf8df68d55d
cwan42 <136786922+cwan42@users.noreply.github.com>
2023-06-28 00:38:52 +0800

    Add files via upload

---
+++ Projects/C04/ft_putnbr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan42 <cwan42@student.42singapore.sg>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 00:24:09 by cwan42            #+#    #+#             */
+/*   Updated: 2023/06/28 00:33:56 by cwan42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+		write (1, "-2147483648", 11);
+	else if (nb < 0)
+	{
+		write (1, "-", 1);
+		nb = -nb;
+		ft_putnbr(nb);
+	}
+	else if (nb > 9)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb +48);
+}
+/*
+int	main(void)
+{
+	ft_putnbr(12345);
+	return (0);
+}
+*/
