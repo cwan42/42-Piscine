2e5ff0d7a30b6cdfffb4351aecbf7ef6b22a208b
cwan42 <136786922+cwan42@users.noreply.github.com>
2023-06-28 23:21:00 +0800

    Add files via upload

---
+++ Projects/C02/ft_putstr_non_printable.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan42 <cwan42@student.42singapore.sg>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/28 22:59:50 by cwan42            #+#    #+#             */
+/*   Updated: 2023/06/28 23:19:59 by cwan42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	ft_isprintable(char c)
+{
+	if (c >= 32 && c <= 126)
+		return (1);
+	else
+		return (0);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (ft_isprintable(str[i]) == 1)
+			ft_putchar(str[i]);
+		else
+		{
+			ft_putchar('\\');
+			ft_putchar("0123456789abcdef"[str[i] / 16]);
+			ft_putchar("0123456789abedef"[str[i] % 16]);
+		}
+		i++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_putstr_non_printable("Coucou\ntu vas bien ?\t\rpepe");
+	ft_putchar('\n');
+	ft_putstr_non_printable("");
+}
+*/
