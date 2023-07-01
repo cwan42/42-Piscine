38d03100608e4e8609ce803da18e71510f6cfebb
cwan42 <136786922+cwan42@users.noreply.github.com>
2023-07-02 01:34:23 +0800

    Add files via upload

---
+++ Projects/C00/ft_print_combn.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_combn.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cwan42 <cwan42@student.42singapore.sg>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/02 01:22:44 by cwan42            #+#    #+#             */
+/*   Updated: 2023/07/02 01:33:28 by cwan42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	print(int *tab, int n)
+{
+	int	i;
+	int	low;
+
+	i = 1;
+	low = 1;
+	while (i < n)
+	{
+		if (tab[i - 1] >= tab[i])
+			low = 0;
+		i++;
+	}
+	if (low)
+	{
+		i = 0;
+		while (i < n)
+			ft_putchar(tab[i++] + 48);
+		if (tab[0] < (10 - n))
+		{
+			ft_putchar(',');
+			ft_putchar(' ');
+		}
+	}
+}
+
+void	ft_print_combn(int n)
+{
+	int	i;
+	int	tab[n];
+
+	i = 0;
+	while (i < n)
+		tab[i++] = 0;
+	while (tab[0] <= (10 - n) && n >= 1 && n <10)
+	{
+		print(tab, n);
+		tab[n - 1]++;
+		i = n -1;
+		while (i && n > 1)
+		{
+			if (tab[i] > 9)
+			{
+				tab[i - 1]++;
+				tab[i] = 0;
+			}
+			i--;
+		}
+	}	
+}
+
+int	main(void)
+{
+	ft_print_combn(2);
+	return (0);
+}
