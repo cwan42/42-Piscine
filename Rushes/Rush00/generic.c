8c6411e214f9b715fd984c12b72e3984c4ec6b1f
cwan42 <136786922+cwan42@users.noreply.github.com>
2023-07-04 15:28:51 +0800

    Add files via upload

---
+++ Rushes/Rush00/generic.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   generic.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgama <mgama@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 13:51:04 by mgama             #+#    #+#             */
+/*   Updated: 2023/07/02 19:18:44 by cwan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "generic.h"
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+	{
+		write(1, str++, 1);
+	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int	ft_atoi(char *str)
+{
+	int				i;
+	unsigned int	nbr;
+	int				sign;
+
+	i = 0;
+	nbr = 0;
+	sign = 0;
+	while (str[i] == '-' || str[i] == '+')
+		if (str[i++] == '-')
+			sign++;
+	while (str[i] >= '0' && str[i] <= '9')
+		nbr = nbr * 10 + (str[i++] - '0');
+	if (sign % 2 == 1)
+		return (nbr * -1);
+	return (nbr);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int	nbr;
+
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nbr = nb * -1;
+	}
+	else
+		nbr = nb;
+	if (nbr / 10 != 0)
+		ft_putnbr(nbr / 10);
+	ft_putchar(nbr % 10 + 48);
+}
