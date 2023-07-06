d7480d5a5306576a9ea0e73d8e2844f9b2677c03
cwan42 <136786922+cwan42@users.noreply.github.com>
2023-07-06 10:40:52 +0800

    Add files via upload

--- Projects/C04/ft_atoi_base.c
+++ Projects/C04/ft_atoi_base.c
@@ -6,21 +6,11 @@
 /*   By: cwan <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 15:27:36 by cwan              #+#    #+#             */
-/*   Updated: 2023/07/05 00:38:58 by cwan42           ###   ########.fr       */
+/*   Updated: 2023/07/06 10:39:34 by cwan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *base)
-{	
-	int	i;
-
-	i = 0;
-	while (base[i] != '\0')
-		i++;
-	return (i);
-}
-
-int	ft_basecheck(char *base)
+int	ft_errorcheck(char *base)
 {
 	int	i;
 	int	j;
@@ -38,44 +28,90 @@ int	ft_basecheck(char *base)
 	while (base[i] != '\0')
 	{
 		j = i + 1;
+		while (base[j] != '\0')
 		{
-			while (base[j] != '\0')
-			{
-				if (base[i] == base[j])
-					return (0);
-				j++;
-			}
-			i++;
+			if (base[i] == base[j])
+				return (0);
+			j++;
 		}
+		i++;
 	}
 	return (1);
 }
 
+int	ft_multr(char d, char *base, int mult, int nbr)
+{
+	int	c;
+
+	c = 0;
+	while (base[c] != '\0')
+	{
+		if (d == base[c])
+			return (nbr + (mult * c));
+		c++;
+	}
+	return (nbr);
+}
+
+int	ft_isbase(char n, char *base)
+{
+	int	c;
+
+	c = 0;
+	if ((n >= '\t' && n <= '\r') || n == ' ' || n == '+' || n == '_')
+		return (1);
+	while (base[c] != '\0')
+	{
+		if (base[c] == n)
+			return (2);
+		c++;
+	}
+	return (0);
+}
+
+int	ft_atoi_simple(char *str, char *base, int size, int start)
+{
+	int	c;
+	int	s;
+	int	result;
+	int	multi;
+
+	c = start - 1;
+	s = 1;
+	result = 0;
+	multi = 1;
+	while (c >= 0)
+	{
+		if (str[c] == '-')
+			s *= (-1);
+		if (ft_isbase(str[c], base) == 2)
+		{
+			result = ft_multr(str[c], base, multi, result);
+			multi *= size;
+		}
+		c--;
+	}
+	return (result * s);
+}
+
 int	ft_atoi_base(char *str, char *base)
 {
+	int	size;
 	int	i;
-	int	sign;
-	int	result;
 
+	size = 0;
 	i = 0;
-	sign = 1;
-	result = 0;
-	if (ft_basecheck(base) == 0)
+	if (ft_errorcheck(base) == 0)
 		return (0);
+	while (base[size] != '\0')
+		size++;
 	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r'))
 		i++;
 	while (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
-			sign *= -1;
 		i++;
-	}
 	while (str[i] >= '0' && str[i] <= '9')
-	{
-		result = (result * ft_strlen(base)) + (str[i] - '0');
 		i++;
-	}
-	return (result * sign);
+	return (ft_atoi_simple(str, base, size, i));
 }
 /*
 #include <stdio.h>
@@ -83,10 +119,8 @@ int	ft_atoi_base(char *str, char *base)
 
 int	main(void)
 {
-	printf(ft_atoi_base("101101", "01"));
-	write(1, "\n", 1);
-	printf(ft_atoi_base("ff", "0123456789abcdef"));
-	write(1, "\n", 1);
+	printf("%d\n", ft_atoi_base("101101", "01"));
+	printf("%d\n", ft_atoi_base("ff", "0123456789abcdef"));
 	return (0);
 }
 */
